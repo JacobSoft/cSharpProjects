25d19043fe03affcf832b190c0c34df00a891445
RobertRedford <clarksjake@gmail.com>
2017-07-24 17:09:46 -0700

    Postage calculator
    add challenger for postal calculator

---
+++ ChallengePostalCalculatorHelperMethods/ChallengePostalCalculatorHelperMethods/default.aspx
@@ -0,0 +1,35 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="default.aspx.cs" Inherits="ChallengePostalCalculatorHelperMethods._default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            Postal Calculator<br />
+            <br />
+            Width:
+            <asp:TextBox ID="widthTextBox" runat="server" AutoPostBack="True" OnTextChanged="widthTextBox_TextChanged"></asp:TextBox>
+            <br />
+            Height:
+            <asp:TextBox ID="heightTextBox" runat="server" AutoPostBack="True" OnTextChanged="heightTextBox_TextChanged"></asp:TextBox>
+            <br />
+            Length:
+            <asp:TextBox ID="lengthTextBox" runat="server" AutoPostBack="True" OnTextChanged="lengthTextBox_TextChanged"></asp:TextBox>
+            <br />
+            <br />
+            <asp:RadioButton ID="groundRadio" runat="server" AutoPostBack="True" GroupName="shipmentMethod" OnCheckedChanged="groundRadio_CheckedChanged" Text="Ground" />
+            <br />
+            <asp:RadioButton ID="airRadio" runat="server" AutoPostBack="True" GroupName="shipmentMethod" OnCheckedChanged="airRadio_CheckedChanged" Text="Air" />
+            <br />
+            <asp:RadioButton ID="nextDayRadio" runat="server" AutoPostBack="True" GroupName="shipmentMethod" OnCheckedChanged="nextDayRadio_CheckedChanged" Text="Next Day" />
+            <br />
+            <br />
+            <asp:Label ID="resultLabel" runat="server"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ ChallengePostalCalculatorHelperMethods/ChallengePostalCalculatorHelperMethods/default.aspx.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ChallengePostalCalculatorHelperMethods
+{
+    public partial class _default : System.Web.UI.Page
+    {
+        double widthDimension = 0.0;
+        double heightDimension = 0.0;
+        double lengthDimension = 0.0;
+        double shipping = 0.0;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void widthTextBox_TextChanged(object sender, EventArgs e)
+        {
+            checkIfInputChanged();
+        }
+
+        protected void heightTextBox_TextChanged(object sender, EventArgs e)
+        {
+            checkIfInputChanged();
+        }
+
+        protected void lengthTextBox_TextChanged(object sender, EventArgs e)
+        {
+            checkIfInputChanged();
+        }
+
+        protected void groundRadio_CheckedChanged(object sender, EventArgs e)
+        {
+            checkIfInputChanged();
+        }
+
+        protected void airRadio_CheckedChanged(object sender, EventArgs e)
+        {
+            checkIfInputChanged();
+        }
+
+        protected void nextDayRadio_CheckedChanged(object sender, EventArgs e)
+        {
+            checkIfInputChanged();
+        }
+
+        private void checkIfInputChanged()
+        {
+            if (widthTextBox.Text.Trim().Length != 0
+                && heightTextBox.Text.Trim().Length != 0
+                && lengthTextBox.Text.Trim().Length != 0)
+            {
+                if (!Double.TryParse(widthTextBox.Text, out widthDimension))
+                    return;
+                if (!Double.TryParse(heightTextBox.Text, out heightDimension))
+                    return;
+                if (!Double.TryParse(lengthTextBox.Text, out lengthDimension))
+                    return;
+                calcCost(widthDimension, heightDimension, lengthDimension);
+            }
+
+            else if (widthTextBox.Text.Trim().Length != 0
+                    && heightTextBox.Text.Trim().Length != 0)
+            {
+                if (!Double.TryParse(widthTextBox.Text, out widthDimension))
+                    return;
+                if (!Double.TryParse(heightTextBox.Text, out heightDimension))
+                    return;
+                calcCost(widthDimension, heightDimension);
+            }
+        }
+
+        private void calcCost(double width, double height)
+        {
+            if (groundRadio.Checked)
+            {
+                double shipping = width * height * .15;
+                printShipping(shipping);
+            }
+            else if (airRadio.Checked)
+            {
+                double shipping = width * height * .25;
+                printShipping(shipping);
+            }
+            else if (nextDayRadio.Checked)
+            {
+                double shipping = width * height * .45;
+                printShipping(shipping);
+            }
+        }
+
+        private void calcCost(double width, double height, double length)
+        {
+            if (groundRadio.Checked)
+            {
+                double shipping = width * height * length * .15;
+                printShipping(shipping);
+            }
+            else if (airRadio.Checked)
+            {
+                double shipping = width * height * length * .25;
+                printShipping(shipping);
+            }
+            else if (nextDayRadio.Checked)
+            {
+                double shipping = width * height * length * .45;
+                printShipping(shipping);
+            }
+        }
+
+        private void printShipping (double shipping)
+        {
+            resultLabel.Text = String.Format("Shipping cost: {0:N2}", shipping.ToString());
+        }
+    }
+}
